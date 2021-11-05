5e30ae1caa52fd3caa4d5c1532c22b117c5633e7
Yann Kenneth <yannkenneth@Yanns-MacBook-Pro.local>
2021-11-05 22:08:24 +0100

    change text color in footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- footer.php -->
 <footer class="bg-dark text-center text-lg-start mt-auto">
-  <div class="text-center p-3">
-    © 2021 Copyright:
+  <div class="text-center text-light p-3">
+    © 2021 Copyright -
     <a class="text-light" href="#">Yann Attakoui</a>
   </div>
 </footer>
