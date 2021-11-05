3cfddd13614235ca7a00f337cb3237fce5d8430f
Yann Kenneth <yannkenneth@Yanns-MacBook-Pro.local>
2021-11-05 22:21:28 +0100

    merge footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,7 @@
 <!-- footer.php -->
-<<<<<<< HEAD
 <footer class="bg-dark text-center text-lg-start mt-auto">
   <div class="text-center text-light p-3">
     © 2021 Copyright -
     <a class="text-light" href="#">Yann Attakoui</a>
-=======
-<footer class="bg-light text-center text-lg-start mt-auto">
-  <div class="text-center p-3">
-    © 2021 Copyright:
-    <a class="text-dark" href="#">HelloCoder</a>
->>>>>>> c1ddddd126f25a0b068a8815a088cb626598636f
   </div>
 </footer>
