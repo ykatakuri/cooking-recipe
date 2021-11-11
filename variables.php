c2addfa5954bbe32436db32f4c41b1bc3bf9eaf9
Yann Kenneth <yannkenneth@Yanns-MacBook-Pro.local>
2021-11-11 19:23:05 +0100

    update variables.php

--- variables.php
+++ variables.php
@@ -13,3 +13,12 @@ if(isset($_GET['limit']) && is_numeric($_GET['limit'])) {
 } else {
     $limit = 100;
 }
+
+if (isset($_COOKIE['LOGGED_USER']) || isset($_SESSION['LOGGED_USER'])) {
+    $loggedUser = [
+        'email' => $_COOKIE['LOGGED_USER'] ?? $_SESSION['LOGGED_USER'],
+    ];
+}
+
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+$rootUrl = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
