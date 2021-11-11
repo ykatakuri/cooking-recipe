4021189910590762c77d2c99ab018f1e3902c0b7
Yann Kenneth <yannkenneth@Yanns-MacBook-Pro.local>
2021-11-11 19:48:35 +0100

    CRUD on the recipes

---
+++ config/user.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_COOKIE['LOGGED_USER']) || isset($_SESSION['LOGGED_USER'])) {
+    $loggedUser = [
+        'email' => $_COOKIE['LOGGED_USER'] ?? $_SESSION['LOGGED_USER'],
+    ];
+} else {
+    throw new Exception('Il faut être authentifié pour ajouter des recettes');
+}
